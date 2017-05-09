51ee29038e63d2ce3cb18d54d415b46edf44c9b7
Abraham <abraham-roos@hotmail.com>
2017-05-09 14:41:34 +0200

    Añadida librería de manipulación de imágenes
    Se ha seguido el tutorial de "http://image.intervention.io/getting_started/installation#laravel" para integrar esta librería a Laravel.

--- petra/config/app.php
+++ petra/config/app.php
@@ -163,6 +163,9 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
+
+
 
         /*
          * Package Service Providers...
@@ -209,6 +212,8 @@ return [
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
+        'Image' => Intervention\Image\Facades\Image::class,
+        'Input' => Illuminate\Support\Facades\Input::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
@@ -228,6 +233,9 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+
+
+
 
     ],
 
