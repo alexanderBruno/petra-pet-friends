e946536d3363947b9b8d2da15895019cf983f0fa
Abraham <abraham-roos@hotmail.com>
2017-05-21 19:08:49 +0200

    Agregada posibilidad de adjuntar imagen en Contact
    Se ha añadido que la posible imagen que un usuario adjunte en el formulario, se envíe junto al mensaje y se reciba en la cuenta de correo.

--- petra/app/Http/Controllers/ContactController.php
+++ petra/app/Http/Controllers/ContactController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Auth, DB, Carbon\Carbon, Mail;
+use Auth, DB, Carbon\Carbon, Mail, Input;
 
 
 class ContactController extends Controller
@@ -30,11 +30,18 @@ class ContactController extends Controller
           array(
               'name' => $request->get('contact_name'),
               'email' => $request->get('contact_email'),
-              'user_message' => $request->get('contact_message')
+              'user_message' => $request->get('contact_message'),
           ), function($message)
       {
-          $message->from('contactepetra@gmail.com');
-          $message->to('contactepetra@gmail.com')->subject('Formulari de contacte Petra');
+        if (Input::hasFile('contact_photo')) {
+          $message->attach(Input::file('contact_photo')->getRealPath(), array(
+              'as' => 'contact_photo.' . Input::file('contact_photo')->getClientOriginalExtension(),
+              'mime' => Input::file('contact_photo')->getMimeType())
+          );
+        }
+        $message->from('contactepetra@gmail.com');
+        $message->to('contactepetra@gmail.com')->subject('Formulari de contacte Petra');
+
       });
 
       return redirect()->action('ContactController@index')->with('confirmation', 'sended');
