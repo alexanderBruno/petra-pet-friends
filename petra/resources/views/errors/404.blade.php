83648af4294b1c922ef09115023242f97f399f8a
Abraham <abraham-roos@hotmail.com>
2017-05-21 18:09:32 +0200

    Añadida imagen a la vista error 404
    Se ha añadido una imagen que aparecerá cuando salte el error 404. Se han hecho unas mini modificaciones a la vista 404 tocando el estilo de los elementos.

--- petra/resources/views/errors/404.blade.php
+++ petra/resources/views/errors/404.blade.php
@@ -5,13 +5,14 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading"><h3>Pàgina no trobada</h3></div>
+            <div class="panel panel-default" style="text-align:center;">
+                <div class="panel-heading"><h3>Error 404: Pàgina no trobada!</h3></div>
 
                 <div class="panel-body">
-                  <h4>Ens sap greu! Aquesta pàgina no existeix en la nostra web.</h4><br/>
+                  <h4>Ens sap greu! Aquesta pàgina no existeix en la nostra web.</h4>
                   <h5>Si creus que es un error, posa't en contacte amb nosaltres.</h5><br/>
-                  <a href="/home"><button type="button" class="btn btn-primary" style="margin-left:40%;"><i class="fa fa-home" aria-hidden="true"></i> Tornar a l'inici</button></a>
+                  <img src="/images/avatars/404img.png" style="width:40%; height:40%;"/><br/><br/>
+                  <a href="/home"><button type="button" class="btn btn-primary"><i class="fa fa-home" aria-hidden="true"></i> Tornar a l'inici</button></a>
                 </div>
 
                 </div>
